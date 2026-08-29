3e18921080d2461a8367f4ec97845fa99e47a438
cihantu-svg <cihantu@gmail.com>
2026-08-29 23:41:03 +0100

    Update fib50_backtest.py

--- fib50_backtest.py
+++ fib50_backtest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-FIB 0.50 KIRILIM BACKTESt
+FIB 0.50 KIRILIM BACKTEST
 
 Senaryo:
   1. 15dk kapanmis mumda (high-low)/open >= %7 (yukari veya asagi)
